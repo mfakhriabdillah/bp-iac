8f3b770eff0c01b3ff1b031d53c811c99d73ec3f
Muhammad Fakhri Abdillah <fakhriabdillah37@gmail.com>
2023-06-21 10:26:05 +0700

    Add Jenkins and Grafana resources

--- main.tf
+++ main.tf
@@ -25,16 +25,65 @@ module "ssh_firewall_rules" {
   target_tags_id   = ["allow-ssh"]
 }
 
+module "http_firewall_rules" {
+  source           = "./modules/network/firewall_rules"
+  project_id       = "studidevops-bigpro-fakhri"
+  network_id       = module.bigpro_vpc.vpc_id
+  firewall_rule_id = "allow-http"
+  ports_allow      = ["80"]
+  target_tags_id   = ["allow-http"]
+}
+
+module "jenkins_firewall_rules" {
+  source           = "./modules/network/firewall_rules"
+  project_id       = "studidevops-bigpro-fakhri"
+  network_id       = module.bigpro_vpc.vpc_id
+  firewall_rule_id = "allow-jenkins"
+  ports_allow      = ["8080"]
+  target_tags_id   = ["allow-jenkins"]
+}
+
+module "grafana_firewall_rules" {
+  source           = "./modules/network/firewall_rules"
+  project_id       = "studidevops-bigpro-fakhri"
+  network_id       = module.bigpro_vpc.vpc_id
+  firewall_rule_id = "allow-grafana"
+  ports_allow      = ["3000"]
+  target_tags_id   = ["allow-grafana"]
+}
+
 module "devops_bastion" {
   source        = "./modules/compute_instance"
   instance_name = "bastion-instance"
   machine_type  = "n1-standard-1"
   zone          = "us-central1-a"
-  tags          = ["allow-ssh"]
+  tags          = ["allow-ssh","allow-http"]
   image         = "ubuntu-os-cloud/ubuntu-2004-lts"
   network       = module.bigpro_vpc.vpc_id
   subnet        = module.bigpro_subnet.subnet_id
   ssh_pubkey = "fakhri:ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDMKsCC8rJUvA8jwemWvMdk2RQ0VheM71DAXqi6tCqg8R8+DUp9p76GudmKOlIe/PnagAaJYTDSQDGujKfugiUObVDOlVrDL8wKrz3sTsXXeg7K8JUiDC4EAYrgf+MW2ETCf2mTKxF30pdU+7ZOLBKwH45spT1ZNyHOHpybG+k859zVzY87XKDsrPL8S1wfe6eET6TrXHuRPyPAkYqbLYseQtu2Ua132NioNcW68So356HTEsRHQrHnc7xHSFol+8NfGy/QRPiTy5zvYhqA8LzolX6K56I3nIA7dQ20GqxrKD+JtZPKEftRYQhPEqvlwPrD6vFV16ZImixNfT6108kbqxYjkbJFB9KK91D1PE2rQovJmYmAzQE8FEK4H7uIwfdr73wdlO0LmGmjopzUpEIxXrysUfhUItW48LjqZgxOxG/jVj4wIRKC7MhFrcLzxnMasZKcWgEWw/ohebRuhmP0tdh03rgb7Ihq09XuCER1Xtxukbg5TP91bjnLI4VDu/c= faseero0@mint"
 }
 
+module "jenkins_instance" {
+  source        = "./modules/compute_instance"
+  instance_name = "jenkins-instance"
+  machine_type  = "n1-standard-1"
+  zone          = "us-central1-a"
+  tags          = ["allow-ssh","allow-jenkins"]
+  image         = "ubuntu-os-cloud/ubuntu-2004-lts"
+  network       = module.bigpro_vpc.vpc_id
+  subnet        = module.bigpro_subnet.subnet_id
+  ssh_pubkey = "fakhri:ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDMKsCC8rJUvA8jwemWvMdk2RQ0VheM71DAXqi6tCqg8R8+DUp9p76GudmKOlIe/PnagAaJYTDSQDGujKfugiUObVDOlVrDL8wKrz3sTsXXeg7K8JUiDC4EAYrgf+MW2ETCf2mTKxF30pdU+7ZOLBKwH45spT1ZNyHOHpybG+k859zVzY87XKDsrPL8S1wfe6eET6TrXHuRPyPAkYqbLYseQtu2Ua132NioNcW68So356HTEsRHQrHnc7xHSFol+8NfGy/QRPiTy5zvYhqA8LzolX6K56I3nIA7dQ20GqxrKD+JtZPKEftRYQhPEqvlwPrD6vFV16ZImixNfT6108kbqxYjkbJFB9KK91D1PE2rQovJmYmAzQE8FEK4H7uIwfdr73wdlO0LmGmjopzUpEIxXrysUfhUItW48LjqZgxOxG/jVj4wIRKC7MhFrcLzxnMasZKcWgEWw/ohebRuhmP0tdh03rgb7Ihq09XuCER1Xtxukbg5TP91bjnLI4VDu/c= faseero0@mint"
+}
 
+module "grafana_instance" {
+  source        = "./modules/compute_instance"
+  instance_name = "grafana-instance"
+  machine_type  = "n1-standard-1"
+  zone          = "us-central1-a"
+  tags          = ["allow-ssh","allow-grafana"]
+  image         = "ubuntu-os-cloud/ubuntu-2004-lts"
+  network       = module.bigpro_vpc.vpc_id
+  subnet        = module.bigpro_subnet.subnet_id
+  ssh_pubkey = "fakhri:ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDMKsCC8rJUvA8jwemWvMdk2RQ0VheM71DAXqi6tCqg8R8+DUp9p76GudmKOlIe/PnagAaJYTDSQDGujKfugiUObVDOlVrDL8wKrz3sTsXXeg7K8JUiDC4EAYrgf+MW2ETCf2mTKxF30pdU+7ZOLBKwH45spT1ZNyHOHpybG+k859zVzY87XKDsrPL8S1wfe6eET6TrXHuRPyPAkYqbLYseQtu2Ua132NioNcW68So356HTEsRHQrHnc7xHSFol+8NfGy/QRPiTy5zvYhqA8LzolX6K56I3nIA7dQ20GqxrKD+JtZPKEftRYQhPEqvlwPrD6vFV16ZImixNfT6108kbqxYjkbJFB9KK91D1PE2rQovJmYmAzQE8FEK4H7uIwfdr73wdlO0LmGmjopzUpEIxXrysUfhUItW48LjqZgxOxG/jVj4wIRKC7MhFrcLzxnMasZKcWgEWw/ohebRuhmP0tdh03rgb7Ihq09XuCER1Xtxukbg5TP91bjnLI4VDu/c= faseero0@mint"
+}
